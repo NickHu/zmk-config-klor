c974be599d8f38bc8fdbb2f9150ad981f8076b07
Nick Hu <me@nickhu.co.uk>
2026-03-27 11:11:09 +0000

    miryoku

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
       url = "github:lilyinstarlight/zmk-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    miryoku-zmk = {
+      url = "github:manna-harbour/miryoku_zmk";
+      flake = false;
+    };
   };
 
   outputs =
@@ -13,49 +17,66 @@
       self,
       nixpkgs,
       zmk-nix,
+      miryoku-zmk,
     }:
     let
       forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
     in
     {
-      packages = forAllSystems (system: rec {
-        default = firmware;
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          default = firmware;
 
-        firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
-          name = "firmware";
+          firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+            name = "firmware";
 
-          src = nixpkgs.lib.sourceFilesBySuffices self [
-            ".board"
-            ".cmake"
-            ".conf"
-            ".defconfig"
-            ".dts"
-            ".dtsi"
-            ".json"
-            ".keymap"
-            ".overlay"
-            ".shield"
-            ".yml"
-            "_defconfig"
-          ];
+            src = pkgs.symlinkJoin {
+              name = "zmk-config-klor";
+              paths = [
+                (nixpkgs.lib.sourceFilesBySuffices self [
+                  ".board"
+                  ".cmake"
+                  ".conf"
+                  ".defconfig"
+                  ".dts"
+                  ".dtsi"
+                  ".json"
+                  ".keymap"
+                  ".overlay"
+                  ".shield"
+                  ".yml"
+                  "_defconfig"
+                ])
+                (pkgs.runCommandLocal "miryoku-zmk" { } ''
+                  mkdir -p $out
+                  ln -s ${miryoku-zmk} $out/miryoku_zmk
+                '')
+              ];
+            };
 
-          board = "nice_nano//zmk";
-          # board = "nrfmicro/nrf52840/zmk";
-          shield = "klor_%PART%";
-          snippets = [ "zmk-usb-logging" ];
+            board = "nice_nano//zmk";
+            # board = "nrfmicro/nrf52840/zmk";
+            shield = "klor_%PART%";
+            # enableZmkStudio = true;
+            snippets = [ ];
 
-          zephyrDepsHash = "sha256-NioLnxuiI3gpgB2wAQUg/V/L/yMBlaob2wZtOdVoHDI=";
+            zephyrDepsHash = "sha256-NioLnxuiI3gpgB2wAQUg/V/L/yMBlaob2wZtOdVoHDI=";
 
-          meta = {
-            description = "ZMK firmware";
-            license = nixpkgs.lib.licenses.mit;
-            platforms = nixpkgs.lib.platforms.all;
+            meta = {
+              description = "ZMK firmware";
+              license = nixpkgs.lib.licenses.mit;
+              platforms = nixpkgs.lib.platforms.all;
+            };
           };
-        };
 
-        flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
-        update = zmk-nix.packages.${system}.update;
-      });
+          flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+          update = zmk-nix.packages.${system}.update;
+        }
+      );
 
       devShells = forAllSystems (system: {
         default = zmk-nix.devShells.${system}.default;
